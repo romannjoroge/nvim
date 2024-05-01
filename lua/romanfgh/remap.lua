b7a0f130719bf675b0e8bda4edc44788ba03fe25
Roman Njoroge <roman.njoroge@njuguna.com>
2024-05-01 11:49:37 +0300

    feat: making remaps get executed when pressed

--- lua/romanfgh/remap.lua
+++ lua/romanfgh/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<C-n>", ":Neotree action=focus")
-vim.keymap.set("n", "<C-g>", ":Neotree action=focus source=git_status")
-vim.keymap.set("n", "<C-t>", ":ToggleTerm")
+vim.keymap.set("n", "<C-n>", function() vim.cmd("Neotree action=focus") end)
+vim.keymap.set("n", "<C-g>", function() vim.cmd("Neotree action=focus source=git_status")end)
+vim.keymap.set("n", "<C-t>", function() vim.cmd("ToggleTerm") end)
 
